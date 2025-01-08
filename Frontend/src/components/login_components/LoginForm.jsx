e903c85326f4fe6efc95f0f28d9163f6f2c387a5
Xcaliblurr_07 <156799501+Xcaliburr-AT@users.noreply.github.com>
2025-01-09 04:01:16 +0800

    Update LoginForm.jsx

--- Frontend/src/components/login_components/LoginForm.jsx
+++ Frontend/src/components/login_components/LoginForm.jsx
@@ -40,7 +40,7 @@ const LoginForm = ({ setUserRole }) => {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://127.0.0.1:8000/api/v1/auth/jwt/create/", {
+            const response = await fetch("https://michjef.pythonanywhere.com/api/v1/auth/jwt/create/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
